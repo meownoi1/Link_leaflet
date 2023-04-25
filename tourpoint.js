69012074a09fac0b050b33fd433da872f4ea4cea
meownoi1 <124670798+meownoi1@users.noreply.github.com>
2023-04-25 15:39:38 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,80 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Bangkok",
+          "type": "Special",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.47518948936477,
+            13.764016091699887
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chonburi",
+          "type": "Special",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.87074650734667,
+            12.968880057823455
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ratchaburi",
+          "type": "Special",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            99.82491429950193,
+            13.540506195065618
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Nakhon ratchasima",
+          "type": "Special",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.09431636210923,
+            14.973820004255842
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chaiyaphum",
+          "type": "์ืNature",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            102.0263830108521,
+            15.817004596789829
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
